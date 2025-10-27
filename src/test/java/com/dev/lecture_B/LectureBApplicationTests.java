bf86d960a17628213eb6faab9bbd764ce47b260d
MinGyeol <alsruf741@naver.com>
2025-10-27 17:12:15 +0900

    방향잡기

---
+++ src/test/java/com/dev/lecture_B/LectureBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.lecture_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
